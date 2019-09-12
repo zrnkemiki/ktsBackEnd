7c345cd98c04b9a6c044cd3916a6daa7867fe656
Ivan Mihajlov <mihajlov.ivan94@gmail.com>
2019-09-12 18:28:38 +0200

    Dodate funkcije za rad sa korisnicima

--- NVT/src/main/java/com/smv/AirSpace/controller/UserController.java
+++ NVT/src/main/java/com/smv/AirSpace/controller/UserController.java
@@ -1,12 +1,18 @@
 package com.smv.AirSpace.controller;
 
+import java.security.Principal;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,13 +28,43 @@ public class UserController {
 
 	@Autowired
 	UserServiceImpl userService;
+	
+	@PreAuthorize("hasAuthority('EMPLOYEE')")
+	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<UserDTO>> getAll() {
+		List<User> users = userService.findAll();
+		List<UserDTO> usersDTO = new ArrayList<>();
+		for (User u : users) {
+			usersDTO.add(new UserDTO(u));
+		}
+		
+		try {
+			return new ResponseEntity<>(usersDTO, HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+	}
+	
+	@GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<UserDTO> getUser(@PathVariable("id") Long id) {
+		User user = userService.findByID(id);
+		if (user == null) {
+			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+		}
+		UserDTO userDTO = new UserDTO(user);
+		return new ResponseEntity<>(userDTO, HttpStatus.OK);
+	}
 
 	// Create new user.
 	@PostMapping(consumes = "application/json")
 	public ResponseEntity<User> addUser(@RequestBody UserDTO userDTO) {
 		User user = userService.saveUser(userDTO);
-
-		return new ResponseEntity<User>(user, HttpStatus.CREATED);
+		return new ResponseEntity<>(user, HttpStatus.CREATED);
+	}
+	
+	@PutMapping()
+	public ResponseEntity<User> updateUser(@RequestBody UserDTO userDTO, Principal principal) {
+		return new ResponseEntity<>(userService.update(userDTO, principal), HttpStatus.OK);
 	}
 
 	// Create new employee.
@@ -36,8 +72,7 @@ public class UserController {
 	@PostMapping(value = "/addEmployee", consumes = "application/json")
 	public ResponseEntity<User> addEmployee(@RequestBody UserDTO userDTO) {
 		User user = userService.saveEmpleyee(userDTO);
-
-		return new ResponseEntity<User>(user, HttpStatus.CREATED);
+		return new ResponseEntity<>(user, HttpStatus.CREATED);
 	}
 
 	@GetMapping(value = "/activate/{uuid}")
--- NVT/src/main/java/exceptions/UserDoesntExistException.java
+++ NVT/src/main/java/exceptions/UserDoesntExistException.java
@@ -4,10 +4,10 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(value= HttpStatus.BAD_REQUEST)
-public class VehicleDoesntExistException extends RuntimeException  {
+public class UserDoesntExistException extends RuntimeException {
 	
-	public VehicleDoesntExistException() {
-		  super("Vozilo sa tim ID-em ne postoji!");
+	public UserDoesntExistException() {
+		  super("Korisnik sa tim ID-em ne postoji!");
 	  }
 	
 	private static final long serialVersionUID = 1L;
